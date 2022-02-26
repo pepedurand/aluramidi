a048c707336d9e1113339de1c011bffe3afc9eb7
Pepe <webpedrod@gmail.com>
2022-02-26 13:35:59 -0300

    Feat: otimização do funcionamento das teclas

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 
-//função que toca o som que recebe um parâmetro que é tocado
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play (); 
+    }
+    else {
+        alert('Elemento não encontrado.');
+    }
 }
 
 
-//varrer lista e achar a tecla
 const listaDeTeclas = document.querySelectorAll ('.tecla');
-let contador = 0;
-//enquanto contador for menor que duração da listadeteclas o contador cresce
-//acrescentando novos itens na array listaDeTeclas
-while (contador < listaDeTeclas.length ) {
 
-    //a var tecla recebe o valor de cada item da lista de teclas 1 a 1
-    const tecla = listaDeTeclas[contador]
-    //a var instrumento recebe o valor de cada item das teclas com sua classe
-    const instrumento = tecla.classList[1];
 
-    //template string
-    const idAudio = `#som_${instrumento}`
-    //ao clicar na tecla (variavel definida), ativa a função anônima
-    //a função toca um som de acordo com a concatenção de #som_ com o instrumento de cada item da array
+for (let contador=0; contador < listaDeTeclas.length; contador++ ) {
+
+    const tecla = listaDeTeclas[contador]   
+    const instrumento = tecla.classList[1];    
+    const idAudio = `#som_${instrumento}` 
+
     tecla.onclick = function () {
         tocaSom (idAudio);
     }
-    contador = contador + 1;
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
